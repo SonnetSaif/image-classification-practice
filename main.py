9d1f2129780f7dcf1e3470312e6f36b27b67152b
sonnetsaif <islam.saiful03@outlook.com>
2025-11-30 22:37:48 +0600

    coco parsing

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+from coco_parser import CocoParser
+
+# Load the JSON file
+json_file_path = '/kaggle/input/plastic-paper-garbage-bag-synthetic-images/ImageClassesCombinedWithCOCOAnnotations/coco_instances.json'
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+parser = CocoParser(data)
+
+df_final = parser.get_final_dataframe()
+
+print(df_final.head())
