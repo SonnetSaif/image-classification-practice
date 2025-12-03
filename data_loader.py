bcc9adad1edf397d6973524ca0078f1fce6f690b
sonnetsaif <islam.saiful03@outlook.com>
2025-12-03 19:48:52 +0600

    data loader

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import os
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
@@ -11,17 +11,45 @@ class CustomDataLoader(torch.utils.data.Dataset):
         self.base_path = base_path
 
     def __len__(self):
-        return len(self.df)
+        return len(self.indices)
     
     def __getitem__(self, idx):
-        row = self.dataframe.iloc[idx]
-        img_path = f"{self.image_dir}/{row['file_name']}"
-        image = Image.open(img_path).convert("RGB")
+        img_id = self.unique_imgs[self.indices[idx]]
+
+        if img_id > 5000:
+            print(f"Skipping image_id: {img_id}")
+            return None, None
         
-        if self.transform:
-            image = self.transform(image)
+        print(f"Processing image_id: {img_id}")
+
+        img_data = self.df[self.df['img_id'] == img_id]
+
+        if img_data.empty:
+            print(f"No data found for image_id: {img_id}")
+            return None, None
         
-        bbox = torch.tensor([row['x1'], row['y1'], row['x2'], row['y2']], dtype=torch.float32)
-        label = torch.tensor(row['category_id'], dtype=torch.long)
+        boxes = img_data[['x1', 'y1', 'x2', 'y2']].values.astype('float32')
+        category_name  = img_data['category_name'].values[0].astype('int64')
+        file_name = img_data['file_name'].values[0]
+
+        file_path = os.path.join(self.base_path, 'Bag Classes', 'Bag Classes', category_name + ' Bag Images', file_name)
         
-        return image, bbox, label
+        if not os.path.exists(file_path):
+            print(f"File not found: {file_path}")
+            return None, None
+        
+        # Load the image
+        img = Image.open(file_path).convert('RGB')
+
+        # Create labels for bounding boxes
+        labels = torch.ones(boxes.shape[0], dtype=torch.int64)
+
+        # Prepare the target dictionary with boxes and labels
+        target = {}
+        target['boxes'] = torch.as_tensor(boxes, dtype=torch.float32)
+        target['labels'] = labels
+
+        # Convert the image to a tensor
+        img = transforms.ToTensor()(img)
+
+        return img, target
